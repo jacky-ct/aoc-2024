4f27bc460d67ab67677557c8a110f3dbed727717
Jackson Thomas <jacksonchristopherthomas@gmail.com>
2024-12-05 12:57:19 +0000

    lint

--- day1/p1.hs
+++ day1/p1.hs
@@ -1,30 +1,20 @@
 import Data.List (sort)
 
--- Pure function to process the lists
 processLists :: [(Int, Int)] -> [Int]
-processLists pairs = 
-    let (listA, listB) = unzip pairs
-        sortedA = sort listA
-        sortedB = sort listB
-    in zipWith (\x y -> abs (x - y)) sortedA sortedB
+processLists pairs =
+  let (listA, listB) = unzip pairs
+      sortedA = sort listA
+      sortedB = sort listB
+   in zipWith (\x y -> abs (x - y)) sortedA sortedB
 
--- IO function to read and process files
 main :: IO ()
 main = do
-    -- Read the file
-    contents <- readFile "input.txt"
-    
-    -- Parse the input
-    let pairs = map parseLine (lines contents)
-    
-    -- Apply pure processing function
-    let differences = sum $ processLists pairs
-    
-    -- Print results
-    print differences
+  contents <- readFile "input.txt"
+  let pairs = map parseLine (lines contents)
+  let differences = sum $ processLists pairs
+  print differences
 
--- Pure parsing function
 parseLine :: String -> (Int, Int)
-parseLine line = 
-    let [a, b] = map read (words line)
-    in (a, b)
+parseLine line =
+  let [a, b] = map read (words line)
+   in (a, b)
